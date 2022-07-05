ed6649c21185d8963c924982fbfdd9004dda3a92
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-05 18:31:16 +0200

    C1: Upload the finished C1 block

---
+++ C1 - Introduction to Programming and Software Design/Units2-3/Task2/ClaseInteger1/src/ClaseInteger.java
@@ -0,0 +1,31 @@
+
+public class ClaseInteger {
+
+	public static void main(String[] args) {
+        // Creación de un objeto Integer usando el segundo contructor
+        Integer num1 = new Integer("125");
+
+        // Creación de un objeto Integer usando el primer contructor
+        Integer num2 = new Integer(20);
+
+        // Obtención del entero que almacena cada objeto Integer.
+        // Si no se hace esto, las líneas siguientes causarían error
+        // de compilación
+        int n1 = num1.intValue();
+        int n2 = num2.intValue();
+        
+        System.out.println("Suma de " + n1 + " y " + n2 + " vale " + (n1 + n2));
+        
+        if ((n1 + n2) > 130)
+        {
+            n1++;
+        }
+        else
+        {
+            n1--;
+        }
+        
+        System.out.println(n1);
+	}
+
+}
