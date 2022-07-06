e47c6605553b4aa9c5ae114f895a29d07d22c356
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-06 18:40:09 +0200

    C2/U1: Upload Task4's Eclipse Project

---
+++ C2 - Object-Oriented-Programming with Java/U1 - Java Basics/Task4/src/Task4.java
@@ -0,0 +1,22 @@
+
+public class Task4 {
+
+	public static void main(String[] args) {
+		int N = 1;
+
+		System.out.println("Initial value of N = " + N);
+		
+		N = N + 77;
+		
+		System.out.println("N + 77 = " + N);
+		
+		N = N - 3;
+		
+		System.out.println("N - 3 = " + N);
+		
+		N = N * 2;
+		
+		System.out.println("N * 2 = " + N);
+	}
+
+}
