165b75693a8e9b629f9634d51d8ddfee7aedad65
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-06 18:04:14 +0200

    C2/U1: Upload Task1's Eclipse Project

---
+++ C2 - Object-Oriented-Programming with Java/U1 - Java Basics/Task1/src/Task1.java
@@ -0,0 +1,15 @@
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		int m_num_a = 20;
+		int m_num_b = 5;
+		
+		System.out.println(m_num_a + " + " + m_num_b + " = " + (m_num_a + m_num_b));
+		System.out.println(m_num_a + " - " + m_num_b + " = " + (m_num_a - m_num_b));
+		System.out.println(m_num_a + " * " + m_num_b + " = " + (m_num_a * m_num_b));
+		System.out.println(m_num_a + " / " + m_num_b + " = " + (m_num_a / m_num_b));
+		System.out.println(m_num_a + " % " + m_num_b + " = " + (m_num_a % m_num_b));
+	}
+
+}
