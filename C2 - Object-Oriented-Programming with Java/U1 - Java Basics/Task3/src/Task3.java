584d17282b6e63613347dc73543b0ebff245c676
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-06 18:36:38 +0200

    C2/U1: Upload Task3's Eclipse Project

---
+++ C2 - Object-Oriented-Programming with Java/U1 - Java Basics/Task3/src/Task3.java
@@ -0,0 +1,27 @@
+
+public class Task3 {
+
+	public static void main(String[] args) {
+		int X = 20, Y = 10;
+		double N = 3.1415, M = 1.618;
+		
+		System.out.println("X = " + X + "; Y = " + Y + "; N = " + N + "; M = " + M);
+		System.out.println(X + " + " + Y + " = " + (X + Y));
+		System.out.println(X + " - " + Y + " = " + (X - Y));
+		System.out.println(X + " * " + Y + " = " + (X * Y));
+		System.out.println(X + " / " + Y + " = " + (X / Y));
+		System.out.println(X + " % " + Y + " = " + (X % Y));
+		System.out.println(N + " + " + M + " = " + (N + M));
+		System.out.println(N + " - " + M + " = " + (N - M));
+		System.out.println(N + " * " + M + " = " + (N * M));
+		System.out.println(N + " / " + M + " = " + (N / M));
+		System.out.println(N + " % " + M + " = " + (N % M));
+		System.out.println(X + " + " + N + " = " + (X + N));
+		System.out.println(Y + " / " + M + " = " + (Y / M));
+		System.out.println(Y + " % " + M + " = " + (Y % M));
+		System.out.println("2X = " + (2 * X) + "; 2Y = " + (2 * Y) + "; 2N = " + (2 * N) + "; 2M = " + (2 * M));
+		System.out.println(X + " + " + Y + " + " + N + " + " + M + " = " + (X + Y + N + M));
+		System.out.println(X + " * " + Y + " * " + N + " * " + M + " = " + (X * Y * N * M));
+	}
+
+}
